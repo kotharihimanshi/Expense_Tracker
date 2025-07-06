b5abecc3fc17015fd4b07bd3b67157d205a6b577
Himanshi Kothari <140066300+kotharihimanshi@users.noreply.github.com>
2025-07-06 15:52:54 +0530

    Update route.js

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,12 +1,12 @@
 import { serve } from "inngest/next";
 
-import { inngest } from "@/lib/inngest/client";
+import { inngest } from "../../../lib/inngest/client";
 import {
   checkBudgetAlerts,
   generateMonthlyReports,
   processRecurringTransaction,
   triggerRecurringTransactions,
-} from "@/lib/inngest/function";
+} from "../../../lib/inngest/functions";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
